a69a38896e490dbe2db656aa84ababab450f958c
YoshiTa0527 <dfingao0527@gmail.com>
2021-05-19 16:01:53 +0900

    sourcetreeの操作の確認のためのプッシュ

---
+++ Assets/Scripts/tanaka/TanakaY/ScoreManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField] UnityEngine.UI.Text m_text = null;
+    
+
+    private void Start()
+    {
+
+    }
+
+    public void SetColor(int actorNumber)
+    {
+
+        if (actorNumber == 1) m_text.color = Color.blue;
+        else m_text.color = Color.blue; ;
+    }
+}
