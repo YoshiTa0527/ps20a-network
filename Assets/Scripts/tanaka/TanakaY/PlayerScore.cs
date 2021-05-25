87d4ea3195f88a059e103971462affa9a0d35ebe
YoshiTa0527 <dfingao0527@gmail.com>
2021-05-25 11:00:24 +0900

    プレイヤーごとにスコアテキストを表示させた

---
+++ Assets/Scripts/tanaka/TanakaY/PlayerScore.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+/// <summary>
+/// プレイヤーのスコアを記録する
+/// </summary>
+public class PlayerScore : MonoBehaviour
+{
+    int m_totalScore;
+    [SerializeField] Text m_scoreText = null;
+    float m_syncTimer;
+    float m_syncInterval = 0.1f;
+    PhotonView m_view;
+
+    private void Start()
+    {
+        m_totalScore = 0;
+
+        m_view = GetComponent<PhotonView>();
+        if (m_view.IsMine)
+        {
+            m_scoreText.color = Color.blue;
+            m_scoreText.rectTransform.position = new Vector2(50, 50);
+        }
+        else
+        {
+            m_scoreText.color = Color.red;
+            m_scoreText.rectTransform.position = new Vector2(150, 150);
+        }
+
+    }
+
+    private void Update()
+    {
+        if (!m_view.IsMine) return;
+        m_syncTimer += Time.deltaTime;
+        if (m_syncTimer > m_syncInterval)
+        {
+            Debug.Log("更新");
+            m_syncTimer = 0;
+            m_view.RPC("SyncScore", RpcTarget.All);
+        }
+    }
+
+    /// <summary>
+    /// 加点する。弾が呼び出す
+    /// </summary>
+    /// <param name="isMaster"></param>
+    /// <param name="score"></param>
+    public void AddScore(int id, int score)
+    {
+        m_totalScore += score;
+    }
+
+    [PunRPC]
+    void SyncScore()
+    {
+        Debug.Log("Called");
+       
+    }
+
+    void RefreshScoreText()
+    {
+
+    }
+
+}
--- Assets/Scripts/tanaka/TanakaY/ScoreManager.cs
+++ Assets/Scripts/tanaka/TanakaY/ScoreManager.cs
@@ -2,21 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
-    [SerializeField] UnityEngine.UI.Text m_text = null;
-    
+    [SerializeField] Text[] m_texts = null;
+    [SerializeField] float m_refleshInterval = 0.1f;
+    float m_refleshTimer;
+    public GameObject[] m_players;
 
-    private void Start()
+    private void Update()
     {
-
+        m_refleshTimer += Time.deltaTime;
+        if (m_refleshTimer > m_refleshInterval)
+        {
+            m_refleshTimer = 0;
+            RefleshScoreList();
+        }
     }
-
-    public void SetColor(int actorNumber)
+    void RefleshScoreList()
     {
 
-        if (actorNumber == 1) m_text.color = Color.blue;
-        else m_text.color = Color.blue; ;
+
     }
 }
