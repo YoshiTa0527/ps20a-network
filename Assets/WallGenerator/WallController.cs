7135cff808b077c2693f8f8298e9983f5956d6dd
HiraEi <aeblanket@gmail.com>
2021-06-08 12:58:12 +0900

    生成するwallが縦横に移動を設定できるようにした

--- Assets/WallGenerator/WallController.cs
+++ Assets/WallGenerator/WallController.cs
@@ -5,11 +5,20 @@ using Photon.Pun;
 
 public class WallController : MonoBehaviour
 {
-    /// <summary>動く速さ</summary>
-    [SerializeField] float m_moveSpeed = 1f;
+    /// <summary>x軸に動く速さ</summary>
+    [SerializeField] float m_moveSpeedX = 1f;
+
+    /// <summary>y軸に動く速さ</summary>
+    [SerializeField] float m_moveSpeedY = 1f;
+
     /// <summary>生存期間（秒）</summary>
     [SerializeField] float m_lifeTime = 10f;
+
     PhotonView m_view = null;
+
+    /// <summary>
+    /// タイマー
+    /// </summary>
     float m_timer = 0f;
 
     void Start()
@@ -23,7 +32,7 @@ public class WallController : MonoBehaviour
 
         if (m_view && m_view.IsMine)      // 自分が生成したものだけ処理する
         {
-            transform.Translate(m_moveSpeed, 0, 0);
+            transform.Translate(m_moveSpeedX, m_moveSpeedY, 0);
         }
 
         m_timer += Time.deltaTime;
