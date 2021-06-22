c65f90c5df33e3156e40f749006a370c6a95a1fa
YSoejima <gureiosugguto@gmail.com>
2021-06-22 13:17:54 +0900

    敵を殺した時にeventを発行するようにした

--- Assets/Scripts/tanaka/EnemyController.cs
+++ Assets/Scripts/tanaka/EnemyController.cs
@@ -1,12 +1,14 @@
 ﻿using UnityEngine;
 // Photon 用の名前空間を参照する
 using Photon.Pun;
+using ExitGames.Client.Photon;
+using Photon.Realtime;
 
 /// <summary>
 /// 敵を制御するコンポーネント
 /// 設定した方向に進み、設定した時間が経ったら破棄される
 /// </summary>
-public class EnemyController : MonoBehaviour
+public class EnemyController : MonoBehaviourPunCallbacks, IOnEventCallback
 {
     /// <summary>動く速さ</summary>
     [SerializeField] float m_moveSpeed = 1f;
@@ -17,6 +19,8 @@ public class EnemyController : MonoBehaviour
     Rigidbody2D m_rb = null;
     PhotonView m_view = null;
     float m_timer = 0f;
+    bool isDeath = false;
+    [SerializeField] private string messege = " ";
 
     void Start()
     {
@@ -39,6 +43,11 @@ public class EnemyController : MonoBehaviour
         {
             PhotonNetwork.Destroy(this.gameObject);
         }
+
+        if (isDeath)
+        {
+            PhotonNetwork.Destroy(this.gameObject);
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collision)
@@ -48,8 +57,40 @@ public class EnemyController : MonoBehaviour
             if (collision.tag == "Bullet" ||
                 collision.tag == "Player")
             {
-                PhotonNetwork.Destroy(this.gameObject);
+                if (!PhotonNetwork.InRoom || !PhotonNetwork.IsMasterClient)
+                {
+                    Raise();
+                }
+                else
+                {
+                    isDeath = true;
+                }
             }
         }
+    }
+
+    void IOnEventCallback.OnEvent(EventData e)
+    {
+        if ((int)e.Code == 2)
+        {
+            EnemyDestroy();
+        }
+    }
+
+
+    void EnemyDestroy()
+    {
+        isDeath = true;
+    }
+    void Raise()
+    {
+        //Debug.Log("ゲーム終了");
+        byte eventCode = 2;
+        RaiseEventOptions raiseEventOptions = new RaiseEventOptions
+        {
+            Receivers = ReceiverGroup.MasterClient
+        };
+        SendOptions sendOptions = new SendOptions();
+        PhotonNetwork.RaiseEvent(eventCode, messege, raiseEventOptions, sendOptions);
     }
 }
