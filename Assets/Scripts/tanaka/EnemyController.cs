30cc0847905c6bed2f26873e7514a1e4a5fcf645
TsutayaJun <dominopizza.0913@icloud.com>
2021-06-29 14:39:53 +0900

    修正：EnemyControllerをBaseクラスにした

--- Assets/Scripts/tanaka/EnemyController.cs
+++ Assets/Scripts/tanaka/EnemyController.cs
@@ -11,32 +11,30 @@ using Photon.Realtime;
 public class EnemyController : MonoBehaviourPunCallbacks, IOnEventCallback
 {
     /// <summary>動く速さ</summary>
-    [SerializeField] float m_moveSpeed = 1f;
+    [SerializeField] protected float m_moveSpeed = 1f;
     /// <summary>動く方向</summary>
-    [SerializeField] Vector2 m_moveDirection = Vector2.left;
+    [SerializeField] protected Vector2 m_moveDirection = Vector2.left;
     /// <summary>生存期間（秒）</summary>
-    [SerializeField] float m_lifeTime = 10f;
+    [SerializeField] protected float m_lifeTime = 10f;
+    /// <summary>経過時間 </summary>
+    float m_timer = 0f;
+    /// <summary>死んだか </summary>
+    bool isDeath = false;
+
     Rigidbody2D m_rb = null;
     PhotonView m_view = null;
-    float m_timer = 0f;
-    bool isDeath = false;
-    [SerializeField] private string messege = " ";
 
     void Start()
     {
         m_rb = GetComponent<Rigidbody2D>();
         m_view = GetComponent<PhotonView>();
-
-        if (m_view && m_view.IsMine)      // 自分が生成したものだけ処理する
-        {
-            m_rb.velocity = m_moveDirection.normalized * m_moveSpeed;
-        }
     }
 
     void Update()
     {
         if (!m_view || !m_view.IsMine) return;      // 自分が生成したものだけ処理する
-
+        
+        Move();
         m_timer += Time.deltaTime;
 
         if (m_timer > m_lifeTime || isDeath)
@@ -45,37 +43,31 @@ public class EnemyController : MonoBehaviourPunCallbacks, IOnEventCallback
         }
     }
 
+    /// <summary>
+    /// 移動する
+    /// </summary>
+    protected virtual void Move()
+    {
+        m_rb.velocity = m_moveDirection.normalized * m_moveSpeed;
+    }
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (m_view && m_view.IsMine)      // 自分が生成したものだけ処理する
         {
-            if (collision.tag == "Bullet" ||
-                collision.tag == "Player")
+            if (collision.CompareTag("Bullet") ||
+                collision.CompareTag("Player"))
             {
-                if (!PhotonNetwork.InRoom || !PhotonNetwork.IsMasterClient)
+                if (!isDeath)
                 {
-                    if (!isDeath)
+                    if (!PhotonNetwork.InRoom || !PhotonNetwork.IsMasterClient)
                     {
                         Raise();
                         Debug.Log("弾が当たった");
-                    }
-                    else
-                    {
                         return;
                     }
-                }
-                else
-                {
-                    if (!isDeath)
-                    {
-                        isDeath = true;
-                        Debug.Log("弾が当たった");
-                    }
-                    else
-                    {
-                        return;
-                    }
-                    
+                    isDeath = true;
+                    Debug.Log("弾が当たった");
                 }
             }
         }
@@ -89,15 +81,21 @@ public class EnemyController : MonoBehaviourPunCallbacks, IOnEventCallback
         }
     }
 
-
+    /// <summary>
+    /// 敵を消す
+    /// </summary>
     void EnemyDestroy()
     {
         isDeath = true;
     }
+
+    /// <summary>
+    /// イベントを起こす
+    /// </summary>
     void Raise()
     {
-        //Debug.Log("ゲーム終了");
         byte eventCode = 2;
+        string messege = " ";
         RaiseEventOptions raiseEventOptions = new RaiseEventOptions
         {
             Receivers = ReceiverGroup.MasterClient
