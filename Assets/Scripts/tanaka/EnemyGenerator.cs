ea2c6fe62415acdd0f69c042af0c3a5145c2162d
TsutayaJun <dominopizza.0913@icloud.com>
2021-07-06 13:11:19 +0900

    追加：一時的に敵の生成を止める機能を追加した。

--- Assets/Scripts/tanaka/EnemyGenerator.cs
+++ Assets/Scripts/tanaka/EnemyGenerator.cs
@@ -4,7 +4,6 @@ using Photon.Pun;
 using Photon.Realtime;
 using ExitGames.Client.Photon;
 
-
 /// <summary>
 /// 敵を生成するコンポーネント
 /// </summary>
@@ -14,20 +13,40 @@ public class EnemyGenerator : MonoBehaviourPunCallbacks, IOnEventCallback
     [SerializeField] string m_enemyResourceName = "PrefabResourceName";
     /// <summary>敵を生成する間隔（秒）</summary>
     [SerializeField] float m_interval = 1f;
-    float m_timer = 0f;
-    bool isTimeUp = false;
+    float m_intervalTimer = 0f;
+    /// <summary>敵の生成を停止する時間（秒）</summary>
+    [SerializeField] float m_restTime = 3f;
+    float m_restTimeTimer = 0;
+    /// <summary>敵を生成し続ける時間（秒）</summary>
+    [SerializeField] float m_generationTime = 10f;
+    float m_generationTimeTimer = 0;
+    /// <summary>タイムアップになったか</summary>
+    bool m_isTimeUp = false;
 
     void Update()
     {
         // マスタークライアント側でのみ敵を生成する
         if (!PhotonNetwork.InRoom || !PhotonNetwork.IsMasterClient) return;
-        
-        m_timer += Time.deltaTime;
 
-        if (m_timer > m_interval && isTimeUp == false)
+        m_intervalTimer += Time.deltaTime;
+        m_generationTimeTimer += Time.deltaTime;
+
+        if (m_intervalTimer > m_interval && m_isTimeUp == false)
         {
-            m_timer = 0;
-            PhotonNetwork.Instantiate(m_enemyResourceName, this.transform.position, Quaternion.identity);
+            if (m_generationTimeTimer <= m_generationTime)
+            {
+                m_intervalTimer = 0;
+                PhotonNetwork.Instantiate(m_enemyResourceName, this.transform.position, Quaternion.identity);
+            }
+            else
+            {
+                m_restTimeTimer += Time.deltaTime;
+                if (m_restTimeTimer > m_restTime)
+                {
+                    m_generationTimeTimer = 0;
+                    m_restTimeTimer = 0;
+                }
+            }
         }
         else
         {
@@ -46,6 +65,6 @@ public class EnemyGenerator : MonoBehaviourPunCallbacks, IOnEventCallback
 
     void EnemyInstanceStop()
     {
-        isTimeUp = true;
+        m_isTimeUp = true;
     }
 }
