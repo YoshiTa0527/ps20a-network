321f9b847240511c5c2e0b587bf7396747ddb9dc
TsutayaJun <dominopizza.0913@icloud.com>
2021-07-13 12:16:35 +0900

    追加：ゲームシーンのUIを追加した。

---
+++ Assets/Scripts/StartAndWaitingPanel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartAndWaitingPanel : MonoBehaviour
+{
+    [SerializeField] GameObject m_startPanel;
+    [SerializeField] float m_startPanelActiveTime;
+    float m_timer;
+    [SerializeField] GameObject m_waitingPanel;
+    [SerializeField] LimitTimeController m_limitTimeController;
+
+    void Update()
+    {
+        if (m_limitTimeController.IsGameStart)
+        {
+            m_startPanel.SetActive(true);
+            m_waitingPanel.SetActive(false);
+            m_timer += Time.deltaTime;
+            if (m_timer >= m_startPanelActiveTime)
+            {
+                m_startPanel.SetActive(false);
+            }
+        }
+    }
+}
