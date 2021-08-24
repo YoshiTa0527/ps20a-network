074b9e7f4b1e9735ab30950a0623c97785900f98
YoshiTa0527 <dfingao0527@gmail.com>
2021-08-24 10:57:59 +0900

    認証方法が変わったのでプッシュのテストする

---
+++ Assets/Scripts/tanaka/ScrollBackGround.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollBackGround : MonoBehaviour
+{
+    [SerializeField] GameObject m_startPosition;
+    [SerializeField] GameObject m_endPosition;
+    [SerializeField] GameObject m_otherEndPosition;
+    Vector3 m_startPos;
+
+    [SerializeField] float m_spd = 5f;
+    private void Start()
+    {
+        m_startPos = m_startPosition.transform.position;
+    }
+    private void Update()
+    {
+        if (Vector3.Distance(m_startPosition.transform.position, m_endPosition.transform.position) == 0)
+        {
+            this.transform.position = m_otherEndPosition.transform.position;
+        }
+
+        this.transform.position += Vector3.left / m_spd;
+
+    }
+}
