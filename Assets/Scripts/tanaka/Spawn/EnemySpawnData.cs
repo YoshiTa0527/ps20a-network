efff09e2a010bb28a56bc408fd79607da320aa3e
kojima0725 <kojikoji0725kt@gmail.com>
2021-08-24 11:59:07 +0900

    とちゅう

---
+++ Assets/Scripts/tanaka/Spawn/EnemySpawnData.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[CreateAssetMenu(fileName = "EnemySpawnData", menuName = "ScriptableObjects/CreateEnemySpawnData")]
+public class EnemySpawnData : ScriptableObject
+{
+    [SerializeField]
+    List<SpawnData> data;
+
+    public List<SpawnData> Data => data;
+}
+
+public enum ESpawnDataType
+{
+    Wait,
+    Spawn
+}
+
+[Serializable]
+public class SpawnData
+{
+    [SerializeField]
+    ESpawnDataType type;
+    [SerializeField]
+    float wait;
+    [SerializeField]
+    float spawnInterval;
+    [SerializeField, Range(0.0f, 0.1f)]
+    float pos;
+    [SerializeField]
+    int spawnAmount;
+    [SerializeField]
+    string obj;
+
+    public ESpawnDataType Type => type;
+    public float Wait => wait;
+    public float SpawnInterval => spawnInterval;
+    public float Pos => pos;
+    public int SpawnAmount => spawnAmount;
+    public string Obj => obj;
+}
---
+++ Assets/Scripts/tanaka/Spawn/EnemySpawner.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+
+public class EnemySpawner : MonoBehaviour
+{
+    SpawnData data;
+
+    bool seted;
+    bool start;
+    int count;
+
+    float timer;
+    public void SetData(SpawnData data)
+    {
+        this.data = data;
+        seted = true;
+    }
+
+    private void Update()
+    {
+        if (!seted)
+        {
+            return;
+        }
+        if (!start)
+        {
+            if (data.Type == ESpawnDataType.Wait)
+            {
+                Destroy(this.gameObject);
+            }
+            start = true;
+            return;
+        }
+
+        timer += Time.deltaTime;
+        if (timer >= data.SpawnInterval)
+        {
+            timer -= data.SpawnInterval;
+            count++;
+            PhotonNetwork.Instantiate(data.Obj, this.transform.position, Quaternion.identity);
+            if (count >= data.SpawnAmount)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+
+    }
+}
