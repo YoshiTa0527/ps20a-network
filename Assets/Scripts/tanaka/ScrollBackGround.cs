ab732360be2223a7f3e2a7b939dd771a339e5d17
YoshiTa0527 <dfingao0527@gmail.com>
2021-08-31 11:46:52 +0900

    完成

--- Assets/Scripts/tanaka/ScrollBackGround.cs
+++ Assets/Scripts/tanaka/ScrollBackGround.cs
@@ -4,27 +4,31 @@ using UnityEngine;
 
 public class ScrollBackGround : MonoBehaviour
 {
-    [SerializeField] GameObject m_startPosition;
-    [SerializeField] GameObject m_endPosition;
-    [SerializeField] GameObject m_otherEndPosition;
-    Vector3 m_startPos;
-    Vector3 m_camelaRectMin;
+    /// <summary>初期位置</summary>
+    float m_startPos;
     [SerializeField] SpriteRenderer m_backGround;
     SpriteRenderer m_backGroundClone;
+    /// <summary>スクロールするスピード</summary>
     [SerializeField] float m_spd = 5f;
     private void Start()
     {
+        m_startPos = m_backGround.transform.position.x;
         m_backGroundClone = Instantiate(m_backGround);
-        m_backGroundClone.transform.Translate(0f, m_backGround.bounds.size.y, 0f);
+        m_backGroundClone.transform.Translate(m_backGround.bounds.size.x, m_backGround.transform.position.y, 0f);
     }
     private void Update()
     {
-
         this.transform.Translate(Vector2.left * m_spd * Time.deltaTime);
-        if (transform.position.x < (m_camelaRectMin.x - Camera.main.transform.position.x) * 2)
+        m_backGroundClone.transform.transform.Translate(Vector2.left * m_spd * Time.deltaTime);
+
+        if (m_backGround.transform.position.x < m_startPos - m_backGround.bounds.size.x)
         {
-            transform.position = new Vector2((Camera.main.transform.position.x - m_camelaRectMin.x) * 2, transform.position.y);
+            m_backGround.transform.Translate(m_backGround.bounds.size.x * 2, 0, 0);
         }
 
+        if (m_backGroundClone.transform.position.x < m_startPos - m_backGroundClone.bounds.size.x)
+        {
+            m_backGroundClone.transform.Translate(m_backGroundClone.bounds.size.x * 2, 0, 0);
+        }
     }
 }
