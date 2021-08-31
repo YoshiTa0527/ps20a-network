732b511748426344bf2e2a9546bea9dcbf81e389
TsutayaJun <dominopizza.0913@icloud.com>
2021-08-31 13:11:18 +0900

    ...デザインを変更中

---
+++ Assets/ui_modifying/ActiveUIButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveUIButton : MonoBehaviour
+{
+    [SerializeField] GameObject m_UI = null;
+    public void OnClick()
+    {
+        m_UI.SetActive(!m_UI.activeSelf);
+    }
+}
