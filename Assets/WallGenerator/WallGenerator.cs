a071615d4ea9101307bf57dfe6210c2344b9db02
HiraEi <aeblanket@gmail.com>
2021-11-08 11:52:44 +0900

    WallGeneratorの修正と、各背景を生成するプレファブを追加

--- Assets/WallGenerator/WallGenerator.cs
+++ Assets/WallGenerator/WallGenerator.cs
@@ -7,40 +7,29 @@ using Photon.Realtime;
 
 public class WallGenerator : MonoBehaviourPunCallbacks, IOnEventCallback
 {
-    /// <summary>
-    /// 生成する壁のオブジェクト
-    /// </summary>
+    /// <summary>インターバルをランダム化するか</summary>
+    [SerializeField] bool m_isRandomInterval = false;
+    /// <summary>生成物のランダム化</summary>
+    [SerializeField] bool m_isRandomGenerator = false;
+    /// <summary>生成する壁のオブジェクト</summary>
     [SerializeField] string[] m_walls;
-
-    /// <summary>
-    /// 生成される壁のインデックス
-    /// </summary>
+    /// <summary>生成される壁のインデックス</summary>
     int m_indexNum = 0;
-
-    /// <summary>
-    /// 生成する間隔
-    /// </summary>
+    /// <summary>生成する間隔</summary>
     [SerializeField] float m_interval = 2f;
-
-    /// <summary>
-    /// タイマー
-    /// </summary>
+    /// <summary>タイマー</summary>
     float m_timer = 0f;
-
-    /// <summary>
-    /// 生成フラグ
-    /// </summary>
+    /// <summary>生成フラグ</summary>
     bool m_generatorFlag = false;
-
-    /// <summary>
-    /// ランダムの最小値
-    /// </summary>
+    /// <summary>ランダムの最小値</summary>
     [SerializeField] float m_intervalMinRange = 2f;
-
-    /// <summary>
-    /// ランダムの最大値
-    /// </summary>
+    /// <summary>ランダムの最大値</summary>
     [SerializeField] float m_intervalMaxRange = 5f;
+
+    void Start()
+    {
+        m_timer = m_interval;
+    }
 
     void Update()
     {
@@ -53,15 +42,27 @@ public class WallGenerator : MonoBehaviourPunCallbacks, IOnEventCallback
             if (m_timer > m_interval)
             {
                 m_timer = 0;
-                if (m_indexNum >= m_walls.Length)
-                {
-                    m_indexNum = 0;
-                }
-
+                
                 PhotonNetwork.Instantiate(m_walls[m_indexNum], this.transform.position, Quaternion.identity);
 
-                m_indexNum++;
-                m_interval = Random.Range(m_intervalMinRange, m_intervalMaxRange);
+                if (m_isRandomGenerator)
+                {
+                    m_indexNum = Random.Range(0, m_walls.Length);
+                }
+                else
+                {
+                    m_indexNum++;
+
+                    if (m_indexNum >= m_walls.Length)
+                    {
+                        m_indexNum = 0;
+                    }
+                }
+                
+                if (m_isRandomInterval)
+                {
+                    m_interval = Random.Range(m_intervalMinRange, m_intervalMaxRange);
+                }
             }
         }
     }
